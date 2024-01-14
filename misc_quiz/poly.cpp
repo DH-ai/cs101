ca601ab8efb76f6f3b2511a52a7eb832d55bb49b
Dhruv Chaturvedi <dhruvastro67@gmail.com>
2024-01-14 20:31:21 +0530

    added the misc/quiz folder for fun and quiz problems

---
+++ misc_quiz/poly.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int poly_func(int n,int &x, int a[], int i = 0){
+    if (n==0){
+        return a[i];
+    }
+    // printf("%d x",a[i]);
+    i++;
+    return a[i-1]+ x*(poly_func(n-1,x,a,i));
+
+}
+int main (){
+    int n;
+    int x;
+    printf("give me the degree of the polynomial");
+    cin>>n;
+    printf("give me the integer x");
+    cin>>x;
+    int a[n+1];
+    for(int i=0; i<=n; i++)
+    {
+        cout<<"enter a"<<i<<"\n"<<endl;
+        cin>>a[i];
+        
+    }
+    
+    
+    
+    int sum = poly_func(n,x,a);
+    cout<<sum<<endl;
+
+}
+
